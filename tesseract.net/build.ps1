55e53c075319a316133199f1f8ca7da9ff03cfe2
ppavlov <petromilpavlov@gmail.com>
2019-08-28 20:41:46 +0300

    Port to .net core

---
+++ tesseract.net/build.ps1
@@ -0,0 +1,7 @@
+﻿Write-Output "Executing build.ps1";
+
+Get-ChildItem -Path "lib\*.dll" -Recurse | Copy-Item -Force
+
+Remove-Item "lib" -Force -Recurse
+
+Write-Output "Finished executing build.ps1";
